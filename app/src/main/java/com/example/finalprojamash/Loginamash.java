720afb9f636ae3395419d80741fadfcb89db2f0c
Student-2024 <student-2024@ashalim.org>
2025-12-01 13:13:04 +0200

    eod 1.12.2025

--- app/src/main/java/com/example/finalprojamash/Loginamash.java
+++ app/src/main/java/com/example/finalprojamash/Loginamash.java
@@ -2,6 +2,8 @@ package com.example.finalprojamash;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -11,11 +13,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Loginamash extends AppCompatActivity {
+import com.example.finalprojamash.services.DatabaseService;
+
+public class Loginamash extends AppCompatActivity implements View.OnClickListener {
 
 
     EditText etEmail, etPassword;
     Button btnSubmit;
+
+    private static final String TAG = "LoginActivity";
+    private DatabaseService databaseService;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +36,72 @@ public class Loginamash extends AppCompatActivity {
             return insets;
         });
 
+        databaseService = DatabaseService.getInstance();
+
         etEmail = findViewById(R.id.et_loginamash_email);
-        etEmail = findViewById(R.id.et_loginamash_password);
+        etPassword = findViewById(R.id.et_loginamash_password);
         btnSubmit = findViewById(R.id.btn_login_submit);
         Button btnBackHome = findViewById(R.id.btnBackHome);
+
+        btnSubmit.setOnClickListener(this);
 
         btnBackHome.setOnClickListener(v -> {
             Intent intent = new Intent(Loginamash.this, MainActivity.class);
             startActivity(intent);
             finish();
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == btnSubmit.getId()) {
+            Log.d(TAG, "onClick: Login button clicked");
+
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            // Validate input
+            if (email.isEmpty()) {
+                etEmail.setError("Enter email");
+              //  etEmail.requestFocus();
+                return;
+            }
+
+            if (password.isEmpty()) {
+                etPassword.setError("Enter password");
+               // etPassword.requestFocus();
+                return;
+            }
+
+            Log.d(TAG, "Logging in with: " + email);
+
+            loginUser(email, password);
+        }
+    }
+
+
+    private void loginUser(String email, String password) {
+
+        Log.d(TAG, "loginUser: Calling Firebase login..."+email+"    "+ password);
+
+        databaseService.LoginUser(email, password, new DatabaseService.DatabaseCallback<String>() {
+            @Override
+            public void onCompleted(String uid) {
+                Log.d(TAG, "User logged in! UID: " + uid);
+
+                Intent mainIntent = new Intent(Loginamash.this, MainActivity.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+            }
+
+            @Override
+            public void onFailed(Exception e) {
+
+                Log.e(TAG, "Firebase login failed", e);
+
+               // etPassword.setError("Invalid email or password");
+               // etPassword.requestFocus();
+            }
         });
     }
 }
